2739849f1c037fdc7d3ee0f3f7d1993eb062176a
Artium <artium89@gmail.com>
2021-07-24 16:47:26 +0300

    Added check that upper bound is not lower than lower bound in Ordered, added tests for Ordered, Sized

--- argscheck/core.py
+++ argscheck/core.py
@@ -12,7 +12,7 @@ class Checker:
     def check(self, *args, **kwargs):
         # Make sure method is called properly and unpack argument's name and value
         if len(args) + len(kwargs) != 1:
-            raise TypeError(f'{self!r}.check() must be called with a single positional argument or a single keyword.'
+            raise TypeError(f'{self!r}.check() must be called with a single positional or keyword argument.'
                             f' Got {len(args)} positional arguments and {len(kwargs)} keyword arguments.')
         if args:
             name, value = 'argument', args[0]
@@ -81,19 +81,7 @@ class Ordered(Checker):
     def __init__(self, *args, lt=None, le=None, ne=None, eq=None, ge=None, gt=None, **kwargs):
         super().__init__(*args, **kwargs)
 
-        if lt is not None and le is not None:
-            raise ValueError(f'Arguments lt={lt!r} and le={le!r} of {self!r}() must not be both provided.')
-
-        if ne is not None and eq is not None:
-            raise ValueError(f'Arguments ne={ne!r} and eq={eq!r} of {self!r}() must not be both provided.')
-
-        if ge is not None and gt is not None:
-            raise ValueError(f'Arguments ge={ge!r} and gt={gt!r} of {self!r}() must not be both provided.')
-
-        if eq is not None and any(p is not None for p in {lt, le, ne, ge, gt}):
-            raise ValueError(f'Argument eq={eq!r} excludes all other arguments of {self!r}.')
-
-        # Arrange arguments in dictionary for easy access
+        # Arrange arguments in a dictionary for convenience
         order_args = dict(lt=lt, le=le, ne=ne, eq=eq, ge=ge, gt=gt)
 
         # Check that arguments are numbers or None
@@ -101,11 +89,38 @@ class Ordered(Checker):
             if value is not None and not isinstance(value, (int, float)):
                 raise TypeError(f'Argument {name}={value!r} of {self!r}() must be a number or None.')
 
+        if (lt is not None) and (le is not None):
+            raise TypeError(f'Arguments lt={lt!r} and le={le!r} of {self!r}() must not be both provided.')
+
+        if (ne is not None) and (eq is not None):
+            raise TypeError(f'Arguments ne={ne!r} and eq={eq!r} of {self!r}() must not be both provided.')
+
+        if (ge is not None) and (gt is not None):
+            raise TypeError(f'Arguments ge={ge!r} and gt={gt!r} of {self!r}() must not be both provided.')
+
+        if (eq is not None) and any(p is not None for p in {lt, le, ne, ge, gt}):
+            raise TypeError(f'Argument eq={eq!r} excludes all other arguments of {self!r}.')
+
+        # Check that lower bound is indeed lower than upper bound (if both are provided)
+        lb = self._get_not_none(ge, gt)
+        ub = self._get_not_none(le, lt)
+        if (lb is not None) and (ub is not None) and (lb > ub):
+            raise ValueError(f'Lower bound {lb!r} of {self!r} must be lower than the upper bound {ub!r}.')
+
         # Create order checker functions for the arguments that are not None
         self.checker_fns = [partial(self._check_order, other=value, **self.orders[name])
                             for name, value
                             in order_args.items()
                             if value is not None]
+
+    @staticmethod
+    def _get_not_none(x, y):
+        if x is not None:
+            return x
+        if y is not None:
+            return y
+
+        return None
 
     @staticmethod
     def _check_order(name, value, other, order_fn, order_name):
--- tests/test_core.py
+++ tests/test_core.py
@@ -32,9 +32,11 @@ class TestCheckerLike(unittest.TestCase):
         # Non supported arguments
         with self.assertRaises(TypeError):
             CheckerLike().check(None)
+        with self.assertRaises(TypeError):
             CheckerLike().check(1)
+        with self.assertRaises(TypeError):
             CheckerLike().check('abcd')
-            CheckerLike().check(tuple())
+        with self.assertRaises(TypeError):
             CheckerLike().check(list())
 
 
@@ -51,7 +53,9 @@ class TestTyped(unittest.TestCase):
         # Non type argument
         with self.assertRaises(TypeError):
             Typed(None)
+        with self.assertRaises(TypeError):
             Typed(1)
+        with self.assertRaises(TypeError):
             Typed(tuple())
 
         # Allowed arguments
@@ -85,4 +89,99 @@ class TestTyped(unittest.TestCase):
 
         with self.assertRaises(TypeError):
             Typed(bool).check(1)
+        with self.assertRaises(TypeError):
             Typed(str).check(0x1234)
+
+
+class TestOrdered(unittest.TestCase):
+    def test_init(self):
+        # Good arguments
+        Ordered()
+        Ordered(le=1.0)
+        Ordered(gt=-4)
+        Ordered(ge=-4, lt=10.4)
+        Ordered(ge=-99.9, lt=-10, ne=-50)
+        Ordered(eq=3)
+
+        # Bad arguments
+        with self.assertRaises(TypeError):
+            Ordered(le='abcd')
+        with self.assertRaises(TypeError):
+            Ordered(eq=3, lt=1)
+        with self.assertRaises(ValueError):
+            Ordered(ge=4, le=3)
+
+    def test_check(self):
+        value = 1e5
+        ret = Ordered().check(value)
+        self.assertIs(value, ret)
+
+        value = 3.14
+        ret = Ordered(gt=3.0, ne=3.1, le=3.14).check(value)
+        self.assertIs(value, ret)
+
+        value = 1
+        ret = Ordered(ne=0).check(value)
+        self.assertIs(value, ret)
+
+        value = -19.0
+        ret = Ordered(eq=-19).check(value)
+        self.assertIs(value, ret)
+
+        value = -19.0
+        ret = Ordered(ge=-19).check(value)
+        self.assertIs(value, ret)
+
+        value = 0.5
+        ret = Ordered(ge=0.0, le=1.0).check(value)
+        self.assertIs(value, ret)
+
+        with self.assertRaises(ValueError):
+            Ordered(ne=4).check(4.0)
+        with self.assertRaises(ValueError):
+            Ordered(eq=4).check(4.1)
+        with self.assertRaises(ValueError):
+            Ordered(gt=3).check(3)
+        with self.assertRaises(ValueError):
+            Ordered(le=5.5).check(6)
+        with self.assertRaises(ValueError):
+            Ordered(ge=0.0, le=1.0).check(1.1)
+
+
+class TestSized(unittest.TestCase):
+    def test_init(self):
+        Sized()
+        Sized(len_eq=5)
+        Sized(len_ge=4)
+        Sized(len_ge=0, len_le=10)
+
+        with self.assertRaises(TypeError):
+            Sized(len_eq='asd')
+        with self.assertRaises(TypeError):
+            Sized(len_eq=1, len_ne=1)
+
+    def test_check(self):
+        value = [1, 2, 3]
+        ret = Sized().check(value)
+        self.assertIs(value, ret)
+
+        value = [1, 2, 3]
+        ret = Sized(len_ge=0).check(value)
+        self.assertIs(value, ret)
+
+        value = [1, 2, 3]
+        ret = Sized(len_ge=0, len_le=3).check(value)
+        self.assertIs(value, ret)
+
+        value = {}
+        ret = Sized(len_eq=0).check(value)
+        self.assertIs(value, ret)
+
+        with self.assertRaises(TypeError):
+            Sized(len_ne=2).check(True)
+
+        with self.assertRaises(ValueError):
+            Sized(len_ne=2).check(dict(a=1, b=2))
+
+        with self.assertRaises(ValueError):
+            Sized(len_lt=3).check({1, 'a', 3.14})
