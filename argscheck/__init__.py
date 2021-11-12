85785bfb686aa11a8170ece91dbaecf566e3d072
Artium <artium89@gmail.com>
2021-11-12 17:09:35 +0200

    Fix package finding issue in setup.cfg

--- argscheck/__init__.py
+++ argscheck/__init__.py
@@ -27,4 +27,4 @@ __all__ = ['check_args', 'Checker', 'Typed', 'One', 'Optional', 'Comparable',
 
            'PathLike', 'ExistingDir', 'ExistingFile']
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
